3fe4567dae11c65793ac457d293bf93d07ea3055
TousifAhamed <tousifahamed11@gmail.com>
2021-03-19 15:01:04 +0300

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -3,6 +3,7 @@
 # save the metrics, parameters
 import os
 import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
@@ -12,7 +13,7 @@ import joblib
 import json
 
 
-def eval_metrics(actual, pred):
+def evaluate_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
@@ -83,8 +84,8 @@ def train_and_evaluate(config_path):
 
 
     
-if __name__="__main__":
+if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    split_and_saved_data(config_path=parsed_args.config)
+    train_and_evaluate(config_path=parsed_args.config)
