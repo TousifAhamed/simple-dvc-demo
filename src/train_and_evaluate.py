af0f4c69d53d17290d5e419d79b51bd4ef42bf78
TousifAhamed <tousifahamed11@gmail.com>
2021-03-19 14:17:12 +0300

    wip train_and_eval

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -11,10 +11,11 @@ import argparse
 import joblib
 import json
 
-def evaluate_metrics(actual, pred):
-    rmse = 
-    mae = 
-    r2 = 
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
 
@@ -48,6 +49,37 @@ def train_and_evaluate(config_path):
 
     predicted_qualities = lr.predict(test_x)
     (rmse, mae, r2) = evaluate_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+#####################################################
+    scores_file = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
+
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+#####################################################
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(lr, model_path)
+
 
 
     
